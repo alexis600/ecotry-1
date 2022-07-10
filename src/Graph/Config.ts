b5d2e10395a89dff5db1794212a1c2d6a4eba1ad
alexis600 <alexis.600@gmail.com>
2022-07-09 23:26:29 -0300

    build
    pre build

--- src/Graph/Config.ts
+++ src/Graph/Config.ts
@@ -1,6 +1,6 @@
 export const config = {
     appId: '7a589e57-ce8c-4766-a325-1d0fc1f0f18e',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: 'http://localhost:4500',
     scopes: [
       'user.read',
       'mailboxsettings.read',
